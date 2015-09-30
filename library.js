4fb1472afaeae6155a806b1fb41a835a989cf8b2
AP <andrea.pegan@h24.it>
2015-09-30 11:48:28 +0200

    Add new link format (permalink.php with get paramaters)

--- library.js
+++ library.js
@@ -2,18 +2,29 @@
     "use strict";
 
     var FacebookEmbed = {},
-        embed = '<p class="fb-post" data-href="https://www.facebook.com/$1/posts/$2" data-width="395"</p>';
-		var embedcode = /<a href="(?:https?:\/\/)?(?:www\.)?(?:facebook\.com)\/(.+)?(?:\/posts\/)(.+)">.+<\/a>/g
+		embed = [
+			{
+				rgxp: /<a href="(?:https?:\/\/)?(?:www\.)?(?:facebook\.com)\/(.+)?(?:\/posts\/)(.+?)".+<\/a>/ig,
+				mbdc: '<p class="fb-post" data-href="https://www.facebook.com/$1/posts/$2" data-width="395"></p>'
+			},
+			{
+				rgxp: /<a href="(?:https?:\/\/)?(?:www\.)?(?:facebook\.com)\/permalink\.php\?story_fbid=(.+?)\&(?:amp;)?id=(.+?)".+<\/a>/ig,
+				mbdc: '<p class="fb-post" data-href="https://www.facebook.com/permalink.php?story_fbid=$1&id=$2" data-width="395"></p>'
+			}
+		];
 	
 	FacebookEmbed.parse = function(data, callback) {
         if (!data || !data.postData || !data.postData.content) {
             return callback(null, data);
         }
-        if (data.postData.content.match(embedcode)) {
-            data.postData.content = data.postData.content.replace(embedcode, embed);
-        }
+		for (var i=0; i<embed.length; i++) {
+	        if (data.postData.content.match(embed[i].rgxp)) {
+    	        data.postData.content = data.postData.content.replace(embed[i].rgxp, embed[i].mbdc);
+        	}
+		}
         callback(null, data);
+    };
 
-    };	
     module.exports = FacebookEmbed;
+
 }(module));
